7af35843a694d45ab36d0802bdf1411dbcaa3d9a
dazega <dazega97@hotmail.com>
2024-01-11 23:31:00 -0600

    feat: creation of get profile endpoint

--- src/controllers/profile.js
+++ src/controllers/profile.js
@@ -1,4 +1,15 @@
 const { Router } = require('express');
+const auth = require('../middlewares/auth');
+const userServices = require('../services/user');
+
 const router = Router();
+
+router.get('/', auth, async (req, res) => {
+    const { user } = req;
+    const userInstance = await userServices.getUserById(user.id);
+    const profile = userInstance.getProfile();
+
+    return res.status(200).json({ profile: profile.getRaw() });
+});
 
 module.exports = router;
